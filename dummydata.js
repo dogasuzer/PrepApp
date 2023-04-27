ad6f57b003c0c1ef9f9de4e4e0db7a9b5dd680e7
dogasuzer <doga.suzer@hotmail.com>
2023-04-27 16:45:21 +0300

    editing the dummy data

--- dummydata.js
+++ dummydata.js
@@ -1,22 +1,22 @@
 export default dummydata = [
   {
     id: 12133,
-    name: "Mayonnaise Chicken sandwich",
+    name: 'Mayonnaise Chicken sandwich',
     steps: [
-      "Cook the chicken: Season chicken breasts with salt and pepper, then cook them in a skillet or on a grill until they are fully cooked.",
-      "Cook the chicken: Season chicken breasts with salt and pepper, then cook them in a skillet or on a grill until they are fully cooked.",
+      'Cook the chicken: Season chicken breasts with salt and pepper, then cook them in a skillet or on a grill until they are fully cooked.',
+      'Add water'
     ],
-    ingredients: ["apple", "banana", "water"],
-    totalTime: "3 hr",
+    ingredients: ['apple', 'banana', 'water'],
+    totalTime: '3 hr'
   },
   {
     id: 121321,
-    name: "Chicken sandwich",
+    name: 'Chicken sandwich',
     steps: [
-      "Cook the chicken: Season chicken breasts with salt and pepper, then cook them in a skillet or on a grill until they are fully cooked.",
-      "Cook the chicken: Season chicken breasts with salt and pepper, then cook them in a skillet or on a grill until they are fully cooked.",
+      'Cook the chicken: Season chicken breasts with salt and pepper, then cook them in a skillet or on a grill until they are fully cooked.',
+      'Cook the chicken: Season chicken breasts with salt and pepper, then cook them in a skillet or on a grill until they are fully cooked.'
     ],
-    ingredients: ["chicken", "bread", "onion"],
-    totalTime: "1 hr",
-  },
+    ingredients: ['chicken', 'bread', 'onion'],
+    totalTime: '1 hr'
+  }
 ];
