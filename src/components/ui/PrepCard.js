f04e701e568ae24a64392bb5457accc08b3a6e4d
dogasuzer <doga.suzer@hotmail.com>
2023-04-28 23:45:43 +0300

    fix: data passing

--- src/components/ui/PrepCard.js
+++ src/components/ui/PrepCard.js
@@ -1,17 +1,14 @@
-import { View, Text } from 'react-native';
-import React, { useState } from 'react';
+import { View } from 'react-native';
+import React from 'react';
 import { StyleSheet } from 'react-native';
-import dummydata from '../../../dummydata';
 import CustomText from './CustomText';
 import IngredientCard from './IngredientCard';
 import COLORS from '../../Colors';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 
-const PrepCard = () => {
+const PrepCard = ({ item }) => {
   const navigation = useNavigation();
-  console.log(dummydata[0]);
-  const item = dummydata[0];
   function onPressHandler() {
     navigation.navigate('PrepDetailScreen', {
       item: item
@@ -19,15 +16,13 @@ const PrepCard = () => {
   }
   return (
     <TouchableOpacity onPress={onPressHandler} style={styles.container}>
-      <CustomText style={styles.prepName}>{dummydata[0].name}</CustomText>
+      <CustomText style={styles.prepName}>{item.name}</CustomText>
 
-      {dummydata !== undefined || typeof dummydata !== 'undefined'
-        ? dummydata[0].ingredients.map(item => {
+      {item !== undefined || typeof item !== 'undefined'
+        ? item.ingredients.map(item => {
             return (
               <View style={styles.ingredientscard}>
                 <IngredientCard item={item} />
-                {/* <ListItem item={item} />
-                  <View style={styles.lineStyle} /> */}
               </View>
             );
           })
